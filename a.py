d75f99354dca4abc416a5444b123883ab9bb73f0
Sambriddhi Shrestha <sambriddhi@Sankalpas-MacBook-Pro.local>
2023-01-21 14:51:32 +0545

    W-9

---
+++ a.py
@@ -0,0 +1,12 @@
+num1 = 10 
+num2 = 20
+if (not (num1<10 and num2 < 10)):
+    print('true')
+else:
+    print (' false ')
+
+if ( not (num2>1 or num1 > num2 -10)):
+    print('true')
+else:
+    print (' false ')
+
